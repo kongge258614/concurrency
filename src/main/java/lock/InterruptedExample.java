64b56d06687ad17ba644b16225fe75b973cf33d1
liliang <dzliliang@126.com>
2019-07-22 18:50:33 +0800

    偏向锁--》轻量级锁--》重量级锁

---
+++ src/main/java/lock/InterruptedExample.java
@@ -0,0 +1,41 @@
+package lock;
+
+/**
+ * @Author liliang
+ * @Date 2019/7/22 18:34
+ * @Description : 测试interrupted
+ **/
+public class InterruptedExample {
+
+    public static void main(String[] args) {
+        System.out.println("主线程执行...........");
+
+        Thread thread1 = new Thread(()->{
+            try {
+                System.out.println("thread1线程开始执行任务...................");
+                Thread.sleep(1000);
+                System.out.println("thread1线程结束执行任务...................");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread1.start();
+
+        Thread thread2 = new Thread(()->{
+            System.out.println("thread2线程执行任务！");
+        });
+        thread2.start();
+
+        Thread thread3 = new Thread(()->{
+            System.out.println("---->thread3线程执行任务！");
+        });
+
+        thread3.start();
+
+
+        thread1.interrupt();
+        System.out.println(thread1.isInterrupted());
+
+
+    }
+}
