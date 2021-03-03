53e15dbb73ec88be3cc8a4aa872e961a64c2d1b4
liliang <liliang@laiyuezs.com>
2021-03-03 10:16:24 +0800

    增加并发工具类

--- src/main/java/concurrencyTools/LockAQS.java
+++ src/main/java/concurrencyTools/LockAQS.java
@@ -1,11 +1,24 @@
 package concurrencyTools;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @Author liliang
  * @Date 2021/3/2 17:44
  * @Description
  **/
 public class LockAQS {
+
+    public static void main(String[] args) {
+        ReentrantLock lock = new ReentrantLock();
+        lock.lock();
+        try {
+            System.out.println("业务逻辑处理............");
+        } finally {
+            lock.unlock();
+        }
+
+    }
 
 
 }
